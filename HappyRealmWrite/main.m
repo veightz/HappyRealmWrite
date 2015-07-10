af159778fa3a1b28ad1ea22a23aee80771f11214
veightz <veightz@gmail.com>
2015-07-11 01:04:52 +0800

    Initial Commit

---
+++ HappyRealmWrite/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HappyRealmWrite
+//
+//  Created by Veight Zhou on 7/11/15.
+//  Copyright (c) 2015 Veight Zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
